f146d705b5dbe4556f1f4f79d1e3abe1c11073c5
Douglas Muth <dmuth@dmuth.org>
2014-03-12 00:23:21 -0400

    Now exiting when requests are done.

--- client.js
+++ client.js
@@ -12,29 +12,45 @@ function main() {
 	commander
 		.option("--num-requests <n>", "Number of requests to make")
 		.option("--concurrency <n>", "How many requests to make in parallel")
+		.option("--url <url>", "The URL of our web server")
 		.parse(process.argv)
 		;
 
-	var url = "http://localhost:3000/";
+	var url = commander.url || "http://localhost:3000/";
+
+	console.log("Hammering URL:", url);
 
 	var pool = generic_pool.Pool({
 		name: "pool",
 		create: function(cb) { cb(); },
-		destory: function(cb) { cb(); },
+		destroy: function() {  },
 		min: 0,
 		max: commander.concurrency
 		});
 
+	var requests_left = commander.numRequests;
+
 	for (var i=0; i<commander.numRequests; i++) {
+
 		pool.acquire(function() {
+
 			request(url, function(error, res, body) {
+
 				if (error) {
 					console.log(error);
 				} else {
-					proccess.stdout.write(".");
+					process.stdout.write(".");
 				}
+
 				pool.release();
+
+				requests_left--;
+				if (requests_left <= 0) {
+					process.exit(0);
+				}
+
 			});
+
 		});
 
 	}
