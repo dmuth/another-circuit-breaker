b8887ef8a1d4b9894b0fb7e4283a8c4f2974f96c
Douglas Muth <dmuth@dmuth.org>
2014-03-12 00:17:21 -0400

    Wrote done() function, created key, and now handling non-200 status codes.

--- server.js
+++ server.js
@@ -13,17 +13,51 @@ var request = require("request");
 */
 function handleRequest(req, res, url) {
 
-	process.stdout.write("C");
+	//
+	// This is something an overly clever node.js programmer might do.
+	// Make the request respond after 500ms through use of setTimeout()
+	// and some clever booleans.
+	//
+
+	var beenhere = false;
+
+	var done = function(response) {
+
+
+		if (beenhere) {
+			//
+			// If we've been here before AND this was called as a response
+			// from request(), then this is a "late" reply.
+			//
+			if (response) {
+				process.stdout.write("L");
+			}
+			return(null);
+		}
+
+		process.stdout.write("R");
+		res.send("Hello", 200);
+		beenhere = true;
+
+	}
+
+	setTimeout(done, 500);
 
 	request(url, function(error, result, body) {
+
 		if (error) {
 			console.log(error);
 			process.stdout.write("E");
 		}
 
-		process.stdout.write("R");
+		if (result.statusCode != 200) {
+			process.stdout.write("5");
+			res.send("Hello", 200);
+			return(null);
+		}
 
-		res.send("Hello", 200);
+		process.stdout.write("D");
+		done(true);
 
 	});
 		
@@ -36,6 +70,7 @@ function handleRequest(req, res, url) {
 function startServer(port, url) {
 
 	var app = express();
+
 	console.log("URL of bad web service is:", url);
 
 	app.get("/", function(req, res) {
@@ -44,6 +79,14 @@ function startServer(port, url) {
 
 	var server = app.listen(port, function() {
 		console.log("Listening on port " + port);
+		console.log();
+		console.log("Key:");
+		console.log("D = Data Received from bad web service");
+		console.log("E = Error received from bad web service");
+		console.log("R = Response sent to client");
+		console.log("L = Late response from bad web service");
+		console.log("5 = Error received from the bad web service");
+		console.log();
 	});
 
 } // End of startServer()
