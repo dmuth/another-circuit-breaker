3adedde502dcd9dd3123c7600aa8c03070bcd525
Douglas Muth <dmuth@dmuth.org>
2014-03-11 23:56:02 -0400

    Split code out into handleRequest().

--- server.js
+++ server.js
@@ -5,30 +5,42 @@ var request = require("request");
 
 
 /**
+* Handle a request that comes in.
+*
+* @param {object} req Our request object
+* @param {object} res Our response object
+* @param {string} url The URL of the bad web service
+*/
+function handleRequest(req, res, url) {
+
+	process.stdout.write("C");
+
+	request(url, function(error, result, body) {
+		if (error) {
+			console.log(error);
+			process.stdout.write("E");
+		}
+
+		process.stdout.write("R");
+
+		res.send("Hello", 200);
+
+	});
+		
+} // End of handleRequest()
+
+
+/**
 * Start up our webserver.
 */
-function startServer(port, remote_port) {
+function startServer(port, url) {
 
 	var app = express();
-	var url = "http://localhost:" + remote_port + "/";
+	console.log("URL of bad web service is:", url);
 
-	app.get("/", function(req, res){
-
-		process.stdout.write("C");
-
-		request(url, function(error, result, body) {
-			if (error) {
-				console.log(error);
-				process.stdout.write("E");
-			}
-
-			process.stdout.write("R");
-
-			res.send("Hello", 200);
-
+	app.get("/", function(req, res) {
+		handleRequest(req, res, url);
 		});
-		
-	});
 
 	var server = app.listen(port, function() {
 		console.log("Listening on port " + port);
@@ -42,9 +54,14 @@ function startServer(port, remote_port) {
 */
 function main() {
 
+	commander
+		.option("--url <url>", "Url of the bad web service")
+		.parse(process.argv)
+		;
+	var url = commander.url || "http://localhost:3001/";
+
 	var port = 3000;
-	var remote_port = 3001;
-	startServer(port, remote_port);
+	startServer(port, url);
 
 } // End of main()
 
