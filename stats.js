267739cd3076f5fbf8ade21b8aa2efb01937d983
Douglas Muth <dmuth@dmuth.org>
2014-03-22 17:22:26 -0400

    Wrote stats module.

---
+++ stats.js
@@ -0,0 +1,62 @@
+/**
+* This package is for keeping track of stats related to the current run of our client (or server).
+*/
+
+//
+// Our stats
+//
+var data = {};
+
+
+/**
+* Increment a stat
+*/
+exports.incr = function(key) {
+
+	if (!data[key]) {
+		data[key] = 0;
+	}
+
+	data[key]++;
+
+} // End of incr()
+
+
+/**
+* Decrement a stat.
+*/
+exports.decr = function(key) {
+
+	if (!data[key]) {
+		data[key] = 0;
+	}
+
+	data[key]--;
+
+} // End of decr()
+
+
+/**
+* Set up a loop to start reporting stats.
+*/
+exports.reportTime = function() {
+
+	var delay = 1 * 1000;
+
+	setTimeout(function() {
+		reportStats();
+	}, delay);
+
+} // End of reportTime()
+
+
+/**
+* Print out our stats, then schedule another run.
+*/
+function reportStats() {
+	console.log(data);
+	exports.reportTime();
+}
+
+
+
