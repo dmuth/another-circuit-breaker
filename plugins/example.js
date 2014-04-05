710e4b3590ada7a387563f3574fd46e139cef85c
Douglas Muth <dmuth@dmuth.org>
2014-04-05 19:39:41 -0400

    Added docs

--- plugins/example.js
+++ plugins/example.js
@@ -12,7 +12,24 @@
 /**
 * Cause our error rate to decay by a specified rate each call.
 *
+* This function is called periodically (likely once per second) by the 
+*	circuit breaker once the number of errors goes above zero.  Once the 
+*	"num-errors" value in the stats is set to zero, the function will 
+*	no longer be called.
+*
 * @param {object} stats Our stats object
+*
+*	The relavent methods in the stats object are listed below:
+*
+*	get(key) - Get a key's value
+*	set(key, value) - Set a key's value
+*	incr(key) - Increment a key's value
+*	decr(key) - Deccrement a key's value
+*	add(key, value) - Add a value to a key
+*	subAbs(key, value) - Subtract a value from a key's value, 
+*		but don't go below zero
+*
+*
 * @param {object} options Our options object
 * @param {function} debug Our debugging funciton
 */
